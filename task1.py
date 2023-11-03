c680b9e281fe2e1ff3471993d780af04392a0d7d
Isabelle Dolan <97686059+IsabelleDolan@users.noreply.github.com>
2023-11-02 20:48:35 -0400

    Calculate acceptable error
    Lines for looking at what the difference in error would be if the timestamp was shifted by 1 second.

--- task1.py
+++ task1.py
@@ -50,18 +50,10 @@ time_max = speed_df['timestamp'].max()
 time_min = speed_df['timestamp'].min()
 ocr_df['timestamp'] = ocr_df['timestamp'].mask(~ocr_df['timestamp'].between(time_min, time_max)) # Where True -> replaced with NaT, so have to do complement
 
-# Option A
-# ocr - mark all rows with non null timestamps
-#non_null_rows = ocr_df['timestamp'].notnull()
-# ocr - get Y travel between non null timestamps from ocr_y
-#ocr_df.loc[non_null_rows, 'y_travel_ocr'] = ocr_df.loc[non_null_rows, 'ocr_y'].diff().abs()
-
-# Option B
 # ocr - make new df with all the non null timestamps
 ocr_non_null_df = ocr_df.dropna().copy()
 # ocr - get Y travel between these non null timestamps from ocr_y
 ocr_non_null_df['ocr_y_move_travel'] = ocr_non_null_df['ocr_y'].diff()*(-1)
-
 
 
 # speed - get Y travel between timestamps in speed
@@ -69,9 +61,10 @@ speed_df['time_diff'] = speed_df['timestamp'].diff().dt.total_seconds()
 speed_df['drum_speed_y_travel'] = (speed_df['drum_speed'] / 60) * speed_df['time_diff'].shift(-1) # Need to shift time_diff back by 1 to line up with drum_speed
 
 # Shift drum speeds down by 1 so that drum speed in row stops at that timestamp in that row
-speed_df['drum_speed_after_this_timestamp'] = speed_df['drum_speed']
-speed_df['drum_speed_before_this_timestamp'] = speed_df['drum_speed'].shift(1)
+speed_df['drum_speed_next'] = speed_df['drum_speed']
+speed_df['drum_speed_previous'] = speed_df['drum_speed'].shift(1)
 
+# REMOVE THIS LATER
 # First remove speed data outside of ocr data. Not necessary but makes it easier to read when I print it out
 time_max = ocr_df['timestamp'].max()
 time_min = ocr_df['timestamp'].min()
@@ -81,63 +74,55 @@ speed_df = speed_df.dropna().copy()
 
 # Merge asof on speed timestamp
 merged_data = pd.merge_asof(speed_df, ocr_non_null_df, on='timestamp', direction='forward')
-merged_data = merged_data[['timestamp', 'drum_speed_after_this_timestamp', 'drum_speed_before_this_timestamp', 'time_diff', 'drum_speed_y_travel', 'ocr_timestamp_move_start', 'ocr_timestamp_move_end', 'ocr_y_at_move_end', 'ocr_y_move_travel']] # Ordering
+merged_data = merged_data[['timestamp', 'drum_speed_next', 'drum_speed_previous', 'time_diff', 'drum_speed_y_travel', 'ocr_timestamp_move_start', 'ocr_timestamp_move_end', 'ocr_y_at_move_end', 'ocr_y_move_travel']] # Ordering
 
 
 # Group the merged_data by ocr_y_at_move_end
-groups = merged_data.groupby('ocr_y_at_move_end')
+groups = merged_data.groupby('ocr_timestamp_move_end')
+
+errors = []
 
 # Loop through the groups
-for y_at_move_end, group in groups:
-    start_time = group['ocr_timestamp_move_start']
-    end_time = group['ocr_timestamp_move_end']
+for group_key, group in groups:
+    start_time = group['ocr_timestamp_move_start'].iloc[0]
+    end_time = group['ocr_timestamp_move_end'].iloc[0]
     start_time_diff = (group['timestamp'].iloc[0] - start_time).total_seconds()
     end_time_diff = (end_time - group['timestamp'].iloc[-1]).total_seconds()
 
-    #Sum middle times. keep array of caculated Y? can als compare directly
+    # Calculate Y travel based on the drum speeds
+    first_move = start_time_diff*group['drum_speed_previous'].iloc[0]/60
+    first_move_false = (start_time_diff+1)*group['drum_speed_previous'].iloc[0]/60 # delete
+    last_move = end_time_diff*group['drum_speed_next'].iloc[-1]/60
+    drum_speed_Y_travel = first_move + last_move
+    drum_speed_Y_travel_off_by_1 = first_move_false + last_move # delete
 
-# Merge speed and orc
-#merged_data = pd.merge_asof(speed_df.dropna(), ocr_non_null_df, on='timestamp')
-#merged_data = merged_data[['timestamp', 'drum_speed', 'time_diff', 'drum_speed_y_travel', 'ocr_timestamp', 'ocr_y', 'ocr_y_travel']] # Reorganizing
+    # If longer than 1 row, need to account for middle rows
+    if len(group) > 1:
+        # Calculate the sum of the Y travel for the middle rows (all but the first)
+        middle_rows = group.iloc[1:]
+        middle_moves = (middle_rows['time_diff'] * middle_rows['drum_speed_next'] / 60).sum()
+        drum_speed_Y_travel = first_move + middle_moves + last_move
 
+        drum_speed_Y_travel_off_by_1 = first_move_false + middle_moves + last_move # delete
 
-# speed - get closest equal to or less than timestamps from speed. Calculate Y travel from times and speeds
-# For timestamp[i] and timestamp[i-1], Y travel = y_diff[i]
-# In speed, Y travel = speed[i-1]*(timestamp[i] - timestamp[i-1])
+    speed_ocr_Y_diff = group['ocr_y_move_travel'].iloc[0] - drum_speed_Y_travel
 
-# Compare Y's. If good, add new column to ocr with True beside good timestamps
-
-
-'''
-#for i in range(len(ocr_non_null_df)):
-for i in range(4):
-    start_time = ocr_non_null_df['timestamp'].iloc[i]
-    end_time = ocr_non_null_df['timestamp'].iloc[i+1]
-    ocr_y_travel = ocr_non_null_df['ocr_y_travel'].iloc[i]
-
-    drum_speed_segments = speed_df[(speed_df['timestamp']>=start_time)&(speed_df['timestamp']<=end_time)]
-    print('start, end')
-    print(start_time, end_time)
-    print(drum_speed_segments)
-'''
+    error = round((drum_speed_Y_travel_off_by_1 - speed_ocr_Y_diff), 4)
 
 
+    if -1 < speed_ocr_Y_diff < 1:
+        errors.append(abs(error))
 
+        #print('speed_ocr_Y_diff')
+        print('-------------')
+        print(round(speed_ocr_Y_diff,4))
+        print(round(drum_speed_Y_travel_off_by_1,4))
+        print(error)
 
+print('Overall min and max tolerances')
+print(min(errors))
+print(max(errors))
 
-print('OCR')
-pd.set_option('display.max_rows', None)
-#print(ocr_df)
-print('SPEED')
-pd.set_option('display.max_rows', 10)
-#print(speed_df)
-print('ocr_non_null_df')
-pd.set_option('display.max_rows', None)
-#print(ocr_non_null_df)
-
-print('MERGED')
-#pd.set_option('display.max_rows', None)
-print(merged_data)
 
 
 '''
@@ -188,8 +173,3 @@ axs[-1].set_xlabel("Timestamp")
 #plt.show()
 
 '''
-
-
-
-
-
